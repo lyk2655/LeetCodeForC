d57efb16e13439ce9c849785245a1ad917f94d99
linyk <linyk3@126.com>
2017-01-17 22:15:30 +0800

    problem 461 hamming distance

---
+++ LeetCodeForC/P461_HammingDistance.c
@@ -0,0 +1,25 @@
+//The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+//Given two integers x and y, calculate the Hamming distance.
+#include "stdio.h"
+#include "string.h"
+int hammingDistance(int x, int y) {
+	if(x == y) {
+		return 0;
+	}
+	int d = 0;
+	for(int i = 0; i < 32; i++) {
+		if(x%2 != y%2) {
+			d++;
+		}
+		x /= 2;
+		y /= 2;
+	}
+	return d;
+}	
+
+int main() {
+	int x,y;
+	printf("the hamming distance is %d", hammingDistance(x,y));
+	return 0;
+}
